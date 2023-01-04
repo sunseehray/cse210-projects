e41cd08352ca263685459e0bd070dfea21e4db58
sunseehray <sabtirazona@byui.edu>
2023-01-04 15:28:13 +0800

    Prep 2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,55 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+                
+        Console.Write("Grade percentage: ");
+        string gradeInput = Console.ReadLine();
+        float gradeNumerical = float.Parse(gradeInput);
+
+        var gradeLetter = "";
+
+        if (gradeNumerical >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeNumerical >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeNumerical >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeNumerical >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else {
+            gradeLetter = "F";
+        }
+
+        // Grade Sign
+
+        var remainder = gradeNumerical % 10;
+        var sign = "";
+
+        if (remainder >=7 && gradeLetter != "A" && gradeLetter != "F")
+        {
+            sign = "+";
+        }
+        else if (remainder <= 3 && gradeLetter != "F")
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"{gradeLetter}{sign}");
+
+        if (gradeNumerical >=70)
+        {
+            Console.WriteLine("You passed.");
+        }
+        else {
+            Console.WriteLine("You failed. Try again.");
+        }
     }
 }
