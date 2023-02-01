3948ae14e25c7d1245ea560e1d78af8f3c18dfb3
sunseehray <sabtirazona@byui.edu>
2023-02-01 16:52:13 +0800

    Learning Activity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,65 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public void InitializeWithNoParameters()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public void InitializeWithNumerator(int numerator)
+    {
+        _top = numerator;
+        _bottom = 1;
+    }
+
+    public void InitializeWithAllParameters(int numerator, int denominator)
+    {
+        _top = numerator;
+        _bottom = denominator;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(this.GetFractionString());
+        Console.WriteLine(this.GetDecimalValue());
+    }
+
+    public int GetTop()
+    {
+        int numerator = _top;
+        return numerator;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        int denominator = _bottom;
+        return denominator;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string fractionString = _top.ToString() + '/' + _bottom.ToString();
+        return fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        double decimalValue = (double)_top / (double)_bottom;
+        return decimalValue;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction1 = new Fraction();
+
+        fraction1.InitializeWithNoParameters();
+        fraction1.Display();
+
+        fraction1.InitializeWithNumerator(5);
+        fraction1.Display();
+
+        fraction1.InitializeWithAllParameters(3,4);
+        fraction1.Display();
+
+        int top = fraction1.GetTop();
+        Console.WriteLine(top);
+        int bottom = fraction1.GetBottom();
+        Console.WriteLine(bottom);
+
+        fraction1.SetTop(1);
+        fraction1.SetBottom(3);
+        fraction1.Display();
     }
 }
