cf02b916e5db4de5725dc791217e3289e6584828
sunseehray <sabtirazona@byui.edu>
2023-03-20 14:54:51 +0800

    finished product, customer, and address

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -4,4 +4,19 @@ public class Customer
 {
     private string _name;
     private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public bool isFromUSA()
+    {
+        if (_address.isUSA() == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
